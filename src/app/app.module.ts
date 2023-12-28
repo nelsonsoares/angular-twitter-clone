ed84c11079d766d5e051e3b84739d10cfe5af5f7
Nelson Soares <nelson.nsf40@gmail.com>
2023-12-28 16:40:29 -0300

    Second Commit: Create component -> home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NewPostComponent } from './components/new-post/new-post.component';
 import { TweetComponent } from './components/tweet/tweet.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NewPostComponent,
-    TweetComponent
+    TweetComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
